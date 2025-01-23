d4f878e1706d6fd73a39850590a6b3b040e72929
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-23 09:54:01 -0800

    Using Express to Retrieve a Get Request Response
    1) When you load a webpage using a browser, it sends an HTTP GET request to the server to fetch the data needed to load the page.
    2) res.send("Server is ready") sends a response to the client in reply to their GET request.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ import express from 'express'; // Import the Express library
 
 const app = express() // Create an instance of an Express application
 
+// Define a route for the root URL ("/") that sends "Server is ready" as a response to GET requests.
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+})
+
 // 5000 (arbitrary choice) is the port number where the Express application will listen for incoming HTTP requests.
 // () => is a callback function (optional) that is executed when the server successfully starts.
 app.listen(5000, () => {
