d8d945634ce2dee077cf28f9f07cec143669cbc1
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-23 10:54:17 -0800

    MongoDB Database Connection on Server Start
    1) Currently, the server starts regardless of whether or not the database is connected successfully.
    2) { connectDB } defines a named import, b/c connectDB is an asynchronous function being imported.

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@
 import express from "express"; // Import the Express library
 // The dotenv library is used to load environment variables from a .env file into your application's process.env object.
 import dotenv from "dotenv"; // Import the dotenv library
+import { connectDB } from './config/db.js'; // Import connectDB async function (named import)
 
 // Loads variables defined in your .env file into the process.env object.
 dotenv.config();
 
 const app = express() // Create an instance of an Express application
-
-console.log(process.env.MONGO_URI);
 
 // Define a route for the root URL ("/") that sends "Server is ready" as a response to GET requests.
 app.get("/products", (req, res) => {
@@ -19,5 +18,6 @@ app.get("/products", (req, res) => {
 // 5000 (arbitrary choice) is the port number where the Express application will listen for incoming HTTP requests.
 // () => is a callback function (optional) that is executed when the server successfully starts.
 app.listen(5000, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000');
 })
