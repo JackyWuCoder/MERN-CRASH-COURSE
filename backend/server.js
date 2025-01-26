55f111bb64640dd082c574ed541c858867093d42
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-26 14:36:29 -0800

    Use Product Model to Delete Product by id

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,12 @@ app.post("/api/products", async (req, res) => {
 
 app.delete("/api/products/:id", async (req, res) => {
     const {id} = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
+
+    }
 })
 
 // 5000 (arbitrary choice) is the port number where the Express application will listen for incoming HTTP requests.
