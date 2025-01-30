106102c62530319cc9cd847242887c24a000efc1
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-29 21:40:44 -0800

    Product API Put Method Error Handling
    1) 500 Status Code for errors related to server.
    2) 404 Status Code for errors related to targeting invalid collection id within a database.

--- backend/server.js
+++ backend/server.js
@@ -58,11 +58,15 @@ app.put("/api/products/:id", async (req, res) => {
 
     const product = req.body;
 
+    if (mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product Id" })
+    }
+
     try {
         const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
         res.status(200).json({ success: true, data: updatedProduct })
     } catch (error) {
-
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 })
 
