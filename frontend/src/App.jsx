3523f10fdf117e7e389244799b83c47036c2c11f
Jacky Wu <JackyWuCoder@gmail.com>
2025-02-01 05:05:57 -0800

    Routing Paths for React Components
    1) path="/" defines the url that will render the Homepage component.
    2) path="/create" defines the url that will render the CreatePage component.
    3) Using routes to render does not refresh the page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-import { Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Box minH="100vh">
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} /> 
+        <Route path="/create" element={<CreatePage />} /> 
+      </Routes>
+    </Box>
   );
 }
 
