eadb3e745f032def51d3b25e8954420b1e608ce3
Jacky Wu <JackyWuCoder@gmail.com>
2025-01-31 16:43:20 -0800

    Define PORT as an Environment Variable
    1) PORT will be an environment variable defined in the .env file.
    2) If the PORT value is undefined then it will use port 5000 that is hard coded.
    3) The PORT will be defined by the hosting service during deployment as the hosting service expect your server to listen on a port they provide.
    4) Setting PORT=5000 as hardcoded is only for local development.

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express() // Create an instance of an Express application
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
@@ -17,7 +18,7 @@ app.use("/api/products", productRoutes);
 
 // 5000 (arbitrary choice) is the port number where the Express application will listen for incoming HTTP requests.
 // () => is a callback function (optional) that is executed when the server successfully starts.
-app.listen(5000, () => {
+app.listen({PORT}, () => {
     connectDB();
     console.log('Server started at http://localhost:5000');
 })
