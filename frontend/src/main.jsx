4865827cdf5de0ea2c336d832893099a70cda6d7
Jacky Wu <JackyWuCoder@gmail.com>
2025-02-01 04:47:34 -0800

    Enabling Routing
    1) Install react-router-dom
    2) Wrap App inside <BrowserRouter>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,17 @@ import React from "react"; // Core React library
 import ReactDOM from "react-dom/client"; // New React 18 API for rendering
 import App from "./App.jsx"; // The main component of application
 import { ChakraProvider } from "@chakra-ui/react"; // Provides Chakra UI's theme, style, and components
+import { BrowserRouter } from "react-router-dom";
 // import system (!!!Required for ChakraProvider to work!!!)
 import { system } from "@chakra-ui/react/preset";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     {/* pass system value here */}
-    <ChakraProvider value={system}>
-      <App />
-    </ChakraProvider>
+      <ChakraProvider value={system}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
